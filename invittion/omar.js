fae688761214dfdb57a75c8667636a01d13e785d
abdallahdolly885-jpg <abdallahdolly885@gmail.com>
2026-05-12 06:03:24 +0300

    Add files via upload

---
+++ invittion/omar.js
@@ -0,0 +1,50 @@
+const openBtn = document.getElementById('openBtn');
+const introScreen = document.getElementById('introScreen');
+const music = document.getElementById('bgMusic');
+const musicBtn = document.getElementById('musicBtn');
+
+/* OPEN INVITATION */
+
+openBtn.onclick = () => {
+
+  music.play();
+
+  introScreen.style.opacity = '0';
+
+  setTimeout(()=>{
+    introScreen.style.display = 'none';
+  },1000)
+
+}
+
+/* MUSIC BUTTON */
+
+musicBtn.onclick = () => {
+
+  if(music.paused){
+    music.play();
+    musicBtn.innerHTML = '♪';
+  }else{
+    music.pause();
+    musicBtn.innerHTML = '🔇';
+  }
+
+}
+
+/* SCROLL ANIMATIONS */
+
+const elements = document.querySelectorAll('.fade-up, .fade-left, .fade-right');
+
+window.addEventListener('scroll',()=>{
+
+  elements.forEach((el)=>{
+
+    const top = el.getBoundingClientRect().top;
+
+    if(top < window.innerHeight - 100){
+      el.classList.add('show');
+    }
+
+  })
+
+})
